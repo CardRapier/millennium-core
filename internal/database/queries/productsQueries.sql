6956cc54286f86b5311d423438e738830e99be97
CardRapier <CardRapier@hotmail.com>
2024-06-15 15:07:21 -0500

    Fix sqlc configuration and add create product query

--- internal/database/queries/productsQueries.sql
+++ internal/database/queries/productsQueries.sql
@@ -1,2 +1,13 @@
 -- name: GetProducts :many
 SELECT * FROM products;
+
+-- name: CreateProduct :exec
+INSERT INTO products (
+    sku,
+    name,
+    description,
+    price,
+    image,
+    stock_quantity,
+    tax_percetange
+) VALUES ($1, $2, $3, $4, $5, $6, $7);
